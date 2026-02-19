4be38c0bfa49ee7adc0e48855999c64a3f0ca002
Deepali T Kottary <deepalikottary07@gmail.com>
2026-02-19 13:28:31 +0530

    update om some bug on the python

--- cat.py
+++ cat.py
@@ -5,17 +5,14 @@ import pandas as pd
 with open("jala.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-# If JSON is a list of products
+# JSON is already a list
 df = pd.DataFrame(data)
 
-# See all column names (important)
 print("Columns in JSON:")
 print(df.columns)
 
-# 🔹 Change 'category' below if your column name is different
 category_column = "category"
 
-# Count products per category
 category_count = df[category_column].value_counts()
 
 print("\nCategory Counts:")
